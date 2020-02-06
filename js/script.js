0ebace88c0d361287f412d7ce75384355558d56c
Igor <faucter@gmail.com>
2020-02-06 19:03:38 +0300

    F

--- js/script.js
+++ js/script.js
@@ -8,12 +8,14 @@ window.addEventListener('DOMContentLoaded', function() {
     title = document.getElementById('title'),
     adv = document.querySelector('.adv'),
     p = document.getElementById('prompt'),
-    btnAccept = document.getElementsByTagName('button')[0];
+    btnAccept = document.getElementsByTagName('button')[0],
+    btnCancel = document.getElementsByTagName('button')[2],
+    btnChange = document.getElementsByTagName('button')[1];
 
     menu.insertBefore(menuItem[2], menuItem[1]);
     document.body.style.background = "url('/img/apple_true.jpg')";
 
-    li.innerHTML = '<li>Пятый пункт</li>';
+    li.innerHTML = '<button>Пятый пункт</button>';
 
     li.classList.add('menu-item');
     menu.appendChild(li);
@@ -51,25 +53,29 @@ window.addEventListener('DOMContentLoaded', function() {
             this.textAlign = textAlign;
             this.text = text;
             }
-        div() { 
+        div() {
             let div = document.createElement('div');
             p.appendChild(div);
             div.textContent = this.text;
-            let param = `
-                height:${this.height}px; 
+            let param = `height:${this.height}px; 
                 width:${this.width}px; 
                 background-color:${this.bg}; 
                 font-size:${this.fontSize}px;
                 text-align:${this.textAlign};`
             ;
             div.style.cssText = param;
+            btnCancel.addEventListener('click', function() {
+                div.remove();
+            });
         }
     }
     
     btnAccept.addEventListener('click', function() {
-        let options = new Options(v1.value, v2.value, v3.value, v4.value, v5.value, v6.value);
+        var options = new Options(v1.value, v2.value, v3.value, v4.value, v5.value, v6.value);
         options.div();
     });
+
+   
     //(width = '200', height = ' ', bg = 'red', text = 'Text', fontSize = '14', color = 'red', textAlign = 'left');
 });
 
